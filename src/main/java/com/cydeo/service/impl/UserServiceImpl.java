b3cc1051bc44e0de6067e7f6d313de2bf43c293c
Boban Milosevic <104642358+bobikop@users.noreply.github.com>
2022-11-03 19:49:09 -0400

    Ticketing App - Data

--- src/main/java/com/cydeo/service/impl/UserServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -1,31 +1,67 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.UserDTO;
+import com.cydeo.entity.Role;
+import com.cydeo.entity.User;
+import com.cydeo.mapper.UserMapper;
+import com.cydeo.repository.UserRepository;
 import com.cydeo.service.UserService;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
 
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper) {
+        this.userRepository = userRepository;
+        this.userMapper = userMapper;
+    }
+
     @Override
     public List<UserDTO> listAllUsers() {
-        return null;
+
+        List<User> userList = userRepository.findAll(Sort.by("firstName"));
+         return  userList.stream()
+                .map(userMapper :: convertToDto)
+                .collect(Collectors.toList());
     }
 
     @Override
     public UserDTO findByUserName(String username) {
-        return null;
+
+        User user = userRepository.findByUserName(username);
+        return userMapper.convertToDto(user);
+
     }
 
     @Override
     public void save(UserDTO user) {
+         userRepository.save(userMapper.convertToEntity(user));
+    }
+
+    @Override
+    public void deleteByUserName(String username) {
+        userRepository.deleteByUserName(username);
 
     }
 
     @Override
-    public void deleteByUserName(Long id) {
+    public UserDTO update(UserDTO user) {
+        //find current user first
+       User user1 = userRepository.findByUserName(user.getUserName()); // has id
+       // map update user dto to entity object
+        User convertedUser = userMapper.convertToEntity(user); // no id and we need to take it form user1 and assigned to user
+        //set id to the converted object
+        convertedUser.setId(user1.getId());
+        userRepository.save(convertedUser);
 
+        return findByUserName(user.getUserName());
     }
 }
