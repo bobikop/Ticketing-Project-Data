6733223be52872e6e4bf3b32203115ef3caf0e0b
Boban Milosevic <104642358+bobikop@users.noreply.github.com>
2022-11-11 17:33:07 -0500

    Ticketing App - Data

--- src/main/java/com/cydeo/service/impl/UserServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -49,7 +49,6 @@ public class UserServiceImpl implements UserService {
     @Override
     public void deleteByUserName(String username) {
 
-
         userRepository.deleteByUserName(username);
 
     }
